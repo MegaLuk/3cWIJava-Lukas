9fefcdd6fa1a85c7cb4bbe6b21a2f105dba4e6b3
Lukas Rauch <lukas.rauch@student.htldornbirn.at>
2021-12-15 08:11:39 +0100

    TikTakToe

--- src/TikTakToe.java
+++ src/TikTakToe.java
@@ -25,15 +25,56 @@ public class TikTakToe {
                             break;
 
                     }
-
-
                 }
-
                 System.out.println();
-
-
             }
-            String selection = scanner.next();
+            int player = counter % 2 +1;
+            System.out.println("Player " + player + " is on the turn!");
+            String [] insert = new String [2];
+            boolean dummheit = true;
+            while(dummheit) {
+                String selection = scanner.next();
+                if(selection.length() == 3) {
+                    insert = selection.split(",");
+                    int value1 = Integer.valueOf(insert[0]);
+                    int value2 = Integer.valueOf(insert[1]);
+                    if(value1 >= 4 || value2 >= 4 || value1 <= 0 || value2 <= 0) {
+                        System.out.println("Nur Zahlen von 1 bis 3 eingeben dieses Feld ist nicht größer!");
+                    } else {
+                        if(feld[value1-1][value2-1] == 0) {
+                            feld[value1-1][value2-1] = player;
+                            dummheit = false;
+                        } else
+                            System.out.println("Dieses Feld ist schon belegt!");
+                    }
+                } else
+                    System.out.println("Falscher Syntax bitte benutze 'reihe,spalte'!");
+            }
+            counter++;
+            gewinner = winner();
+            if (winner())
+                System.out.println("Spieler " + player + " hat Gewonnen!");
+
         }
+
+    }
+    public static boolean winner() {
+        if(feld[0][0] == feld[0][1] && feld[0][0] == feld[0][2] && feld[0][0] != 0)
+            return true;
+        if(feld[1][0] == feld[1][1] && feld[1][0] == feld[1][2] && feld[1][0] != 0)
+            return true;
+        if(feld[2][0] == feld[2][1] && feld[2][0] == feld[2][2] && feld[2][0] != 0)
+            return true;
+        if(feld[0][0] == feld[1][0] && feld[0][0] == feld[2][0] && feld[0][0] != 0)
+            return true;
+        if(feld[0][1] == feld[1][1] && feld[0][1] == feld[2][1] && feld[0][1] != 0)
+            return true;
+        if(feld[0][2] == feld[1][2] && feld[0][2] == feld[2][2] && feld[0][2] != 0)
+            return true;
+        if(feld[0][0] == feld[1][1] && feld[0][0] == feld[2][2] && feld[0][0] != 0)
+            return true;
+        if(feld[2][0] == feld[1][1] && feld[2][0] == feld[0][2] && feld[2][0] != 0)
+            return true;
+        return false;
     }
 }
--- src/schoolcheck/RandomSentence.java
+++ src/schoolcheck/RandomSentence.java
@@ -6,11 +6,13 @@ import java.util.Random;
 public class RandomSentence {
 
     public static void main(String[] args) {
-        String[] teil1 = {"Er","Sie"};
-        String[] teil2 = {"geht","läuft","rennt","fährt"};
-        String[] teil3 = {"nach Hause","zur Schule","ins Büro","zur Party"};
+        String[] subjects = {"Er","Sie"};
+        String[] verbs = {"geht","läuft","rennt","fährt"};
+        String[] objects = {"nach Hause","zur Schule","ins Büro","zur Party"};
 
-        boolean ending = true;
+        boolean theEnd = true;
+
+        System.out.println("Geben Sie eine Zahl ein");
 
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
@@ -18,10 +20,10 @@ public class RandomSentence {
         int selection = scanner.nextInt();
 
         for(int i = 0; i <= selection -1; i++) {
-            int random1 = random.nextInt(1) ;
-            int random2 = random.nextInt(3) ;
-            int random3 = random.nextInt(3) ;
-            System.out.println(teil1[random1] + " " + teil2[random2] + " " + teil3[random3]);
+            int random1 = random.nextInt (subjects.length) ;
+            int random2 = random.nextInt (verbs.length) ;
+            int random3 = random.nextInt(objects.length) ;
+            System.out.println(subjects[random1] + " " + verbs[random2] + " " + objects[random3]);
 
         }
 
